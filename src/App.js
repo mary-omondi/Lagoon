c05e75a410a289a2320d037e056aec9b0e8d1889
mary-omondi <maryomondi263@gmail.com>
2022-10-14 18:08:28 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CreateUsers from './components/createusers';
+import Dropdown from './components/Dropdown';
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <CreateUsers/>
+      <Dropdown/>
+    
+    </div>
+  );
+}
+export default App;
+
